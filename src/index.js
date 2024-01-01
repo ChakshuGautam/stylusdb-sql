4f49e4c7c639a2831e1a95b061ed5f7f5dd2b74a
Chakshu Gautam <chaks.gautam@gmail.com>
2024-01-01 11:05:15 +0530

    Tutorial-04

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const parseQuery = require('./queryParser');
+const readCSV = require('./csvReader');
+
+async function executeSELECTQuery(query) {
+    const { fields, table } = parseQuery(query);
+    const data = await readCSV(`${table}.csv`);
+
+    // Filter the fields based on the query
+    return data.map(row => {
+        const filteredRow = {};
+        fields.forEach(field => {
+            filteredRow[field] = row[field];
+        });
+        return filteredRow;
+    });
+}
+
+module.exports = executeSELECTQuery;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,6 @@
 const readCSV = require('../src/csvReader');
 const parseQuery = require('../src/queryParser');
-
-test('Basic Jest Test', () => {
-    expect(1).toBe(1);
-});
+const executeQuery = require('../src/index');
 
 test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
@@ -20,4 +17,14 @@ test('Parse SQL Query', () => {
         fields: ['id', 'name'],
         table: 'sample'
     });
+});
+
+test('Execute SQL Query', async () => {
+    const query = 'SELECT id, name FROM sample';
+    const result = await executeQuery(query);
+    expect(result.length).toBeGreaterThan(0);
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0]).not.toHaveProperty('age');
+    expect(result[0]).toEqual({ id: '1', name: 'John' });
 });

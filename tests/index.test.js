7a5e8edce8def2c2ad41ad186ae882ae959579eb
Yash Mittal <mittal.yash12@hotmail.com>
2024-01-06 16:00:26 +0530

    feat: working app at the end of step-02.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,9 @@
-test('Basic Jest Test', () => {
-  expect(1).toBe(1);
+const readCSV = require('../src/csvReader');
+
+test('Read CSV File', async () => {
+    const data = await readCSV('./sample.csv');
+    expect(data.length).toBeGreaterThan(0);
+    expect(data.length).toBe(3);
+    expect(data[0].name).toBe('John');
+    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
 });

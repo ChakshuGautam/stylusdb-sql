55b4ba6cdeda0ad85fe7047404bac2c6720ae4fb
Yash Mittal <mittal.yash12@hotmail.com>
2024-01-06 16:08:17 +0530

    feat: working app at the end of step-04.

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,6 @@
 const readCSV = require('../src/csvReader');
 const parseQuery = require('../src/queryParser');
+const executeSELECTQuery = require('../src/index');
 
 test('Read CSV File', async () => {
     const data = await readCSV('./sample.csv');
@@ -16,4 +17,14 @@ test('Parse SQL Query', () => {
         fields: ['id', 'name'],
         table: 'sample'
     });
+});
+
+test('Execute SQL Query', async () => {
+    const query = 'SELECT id, name FROM sample';
+    const result = await executeSELECTQuery(query);
+    expect(result.length).toBeGreaterThan(0);
+    expect(result[0]).toHaveProperty('id');
+    expect(result[0]).toHaveProperty('name');
+    expect(result[0]).not.toHaveProperty('age');
+    expect(result[0]).toEqual({ id: '1', name: 'John' });
 });

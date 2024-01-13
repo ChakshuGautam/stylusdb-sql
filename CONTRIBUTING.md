63d984a0c57cbdfdf854315046cb94d18b718de4
Chakshu Gautam <chaks.gautam@gmail.com>
2024-01-13 14:56:40 +0530

    Fix main in package.json

---
+++ CONTRIBUTING.md
@@ -0,0 +1,1 @@
+By contributing, you agree to release your modifications under the LGPL-2.1 license.
--- demo/www/frontend.js
+++ demo/www/frontend.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:6060");
+const socket = new WebSocket("wss://stylusdb-sql-server.onrender.com");
 
 var term = new window.Terminal({
     cursorBlink: true
